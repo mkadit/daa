fcb15ced6e502b2724a90aad83f1db36366568ba
mkadit <mkaditya00@gmail.com>
2021-12-05 22:31:32 +0700

    Change testings

--- src/main/java/daa/Main.java
+++ src/main/java/daa/Main.java
@@ -4,41 +4,87 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 public class Main {
-	public static void main(String[] args) {
-		// for (int index = 0; index < 10; index++) {
+	static HashMap<String, HashMap<Integer, ArrayList<Long>>> timeAlgo = new HashMap<>();
+	static int index = 0;
+	static int numTestCase = 7;
+	static int numTestDone = 10;
 
-		// }
+	public static void main(String[] args) {
+		// initialization
+		timeAlgo.put("KMPTime", new HashMap<Integer, ArrayList<Long>>());
+		timeAlgo.put("DoubleHashMatching", new HashMap<Integer, ArrayList<Long>>());
+		timeAlgo.put("Bruteforce", new HashMap<Integer, ArrayList<Long>>());
+		timeAlgo.put("RabinKarpMatching", new HashMap<Integer, ArrayList<Long>>());
+		timeAlgo.put("BoyerMooreHorspool", new HashMap<Integer, ArrayList<Long>>());
+		timeAlgo.put("Raita", new HashMap<Integer, ArrayList<Long>>());
+		for (String algo : timeAlgo.keySet()) {
+			for (int index = 0; index <= numTestCase; index++) {
+				timeAlgo.get(algo).put(index, new ArrayList<>());
+			}
+		}
+
+		for (int i = 1; i <= numTestDone; i++) {
+			System.out.println("Iterasi: " + i);
+			begin();
+			// System.out.println(timeAlgo.toString());
+
+		}
+		for (String algo : timeAlgo.keySet()) {
+			System.out.println(algo);
+			for (int i = 0; i < numTestCase; i++) {
+				ArrayList<Long> time = timeAlgo.get(algo).get(i);
+				long min = Collections.min(time);
+				long max = Collections.max(time);
+				double avg = 0;
+				for (long value : time) {
+					avg += value;
+
+				}
+				avg /= numTestDone;
+
+				System.out.println("Test case number: " + i);
+				System.out.println("Minimal Time: " + min);
+				System.out.println("Maximal Time: " + max);
+				System.out.println("Average Time: " + avg);
+				System.out.println();
+
+			}
+		}
+	}
+
+	static void begin() {
 		String text = "Sofita";
 		String pattern = "ofi";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "DAADJSAKAS";
 		pattern = "JSA";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "LALALSDJSKLAJKLDSAJKLJKLKLKAAAXOXJDASLKJDSKLASDALK";
 		pattern = "XOX";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "XIJASDLJLKNCKLNAKLDMQWEPOQWIRJQFLKANVASKGHAJDAKLSJELKQWJYUUYYUYXUYUXYUYULDKASJLYUYUYUYUKDASJLKDJASLKASDJLK";
 		pattern = "YUYUYUYU";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "DKLMEALDMAJKCPEAOKRLEMGKHOELKASNMELKMLAKFMLAJLMKLELQRKJROIQUIJKADWJKLJE";
 		pattern = "ELKMLA";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "DKLMEALDMAJKCPEAOKRLEMGKHOELKASNMELKMLAKFMLAJLMKLELQRKJROIQUIMEALDMAJKCPEAOKRLEMGKHOELKASNMELKMLAKFMLAJLMKLELQRKJROIJKADWJKLJE";
 		pattern = "LKMLAKFMLAJLMKLELQRKJROIQUIMEALDMAJKCPEAOKRLEMGKHOELKASNMELKMLAKFML";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		text = "38120380^%^!#!HJKnkjdshfkahdkcy39479Shechchenkodsdak P1 $ %^ &7ST asdkj128937 97sdf6&^76381687&^*#!*&(*(FASDKJQWHEUYC))";
 		pattern = "Shechchenkodsdak P1 $ %^ &7ST";
-		beginCompare(text, pattern);
+		inputCase(text, pattern);
 
 		Path path = Paths.get("bees.txt").toAbsolutePath();
 		// System.out.println(path.toString());
@@ -47,48 +93,41 @@ public class Main {
 			// System.out.println(content);
 			text = content;
 			pattern = "jazz";
-			beginCompare(text, pattern);
+			inputCase(text, pattern);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		// text = """
-		// """;
+		index = 0;
+
 	}
 
-	static long[] beginCompare(String text, String pattern) {
-		// System.out.println("Matching Text: " + text);
-		// System.out.println("with Pattern: " + pattern);
+	static void inputCase(String text, String pattern) {
+		HashMap<String, Long> result = beginCompare(text, pattern);
+		for (String algo : result.keySet()) {
+			timeAlgo.get(algo).get(index).add(result.get(algo));
+
+		}
+		System.out.println();
+		index++;
+	}
+
+	static HashMap<String, Long> beginCompare(String text, String pattern) {
 		long KMPTime = KMP_String_Matching.matchStringTime(pattern, text);
-		// System.out.println();
 		long DoubleTime = DoubleHashMatching.matchStringTime(pattern, text);
-		// System.out.println();
 		long BruteforceTime = Bruteforce.matchStringTime(pattern, text);
-		// System.out.println();
 		long RabinKarpTime = RabinKarpMatching.matchStringTime(pattern, text, 101);
-		// System.out.println();
 		long BoyerMooreTime = BoyerMooreHorspool.matchStringTime(pattern, text);
-		// System.out.println();
 		long RaitaTime = Raita.matchStringTime(pattern, text);
 
-		long[] times = { KMPTime, DoubleTime, BruteforceTime, RabinKarpTime, BoyerMooreTime, RaitaTime };
-		Arrays.sort(times);
+		HashMap<String, Long> algorighms = new HashMap<>();
+		algorighms.put("KMPTime", KMPTime);
+		algorighms.put("DoubleHashMatching", DoubleTime);
+		algorighms.put("Bruteforce", BruteforceTime);
+		algorighms.put("RabinKarpMatching", RabinKarpTime);
+		algorighms.put("BoyerMooreHorspool", BoyerMooreTime);
+		algorighms.put("Raita", RaitaTime);
 
-		HashMap<Long, String> algorighms = new HashMap<>();
-		algorighms.put(KMPTime, "KMPTime");
-		algorighms.put(DoubleTime, "DoubleHashMatching");
-		algorighms.put(BruteforceTime, "Bruteforce");
-		algorighms.put(RabinKarpTime, "RabinKarpMatching");
-		algorighms.put(BoyerMooreTime, "BoyerMooreHorspool");
-		algorighms.put(RaitaTime, "Raita");
-
-		String fastestAlgorithm = algorighms.get(times[0]);
-		// System.out.println(
-		// "The fastest algorithm with\ntext:" + text + "\nPattern: " + pattern + "\nis:
-		// " + fastestAlgorithm);
-		System.out.println(
-				"The fastest algorithm with" + "\nis: " + fastestAlgorithm);
-		System.out.println();
-		return times;
+		return algorighms;
 
 	}
 
